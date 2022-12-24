77bc7094e2c691e0b1719f059bcfb0b5de3ce6a8
pankajkrbansal <pankajbansal.pkb@gmail.com>
2022-12-24 17:14:46 +0530

    Added routes for users

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,39 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Patch, Post, Put } from '@nestjs/common';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+    @Get('/@:username')
+    getUserbyUserName(@Param() param):string{
+        return `Fetching Details of User ${param.username}`
+    }
+
+    @Get('/:userid')
+    getUserByUserId(@Param() param):string{
+        return `Fetching details Of User id = ${param.userid}`
+    }
+
+    @Post('/')
+    createNewUser(@Body() userBody):string{
+        return ''
+    }
+
+    @Delete('/:userid/follow')
+    unfollowUser():string{
+        return ''
+    }
+
+    @Patch('/:userid')
+    updateUserDetails():string{
+        return ''
+    }
+
+    @Get('/:userid/followers')
+    getuserFollowers(@Param() param):string{
+        return ''
+    }
+
+    @Put('/:userid/followees')
+    addUserFollowers(@Param() param):string{
+        return ''
+    }
+}
