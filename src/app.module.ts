286c419906fb4e8aa309c71e47833efb04140e14
PankajB3 <pankajbansal@rapidinnovation.dev>
2022-12-24 15:17:30 +0530

    Added controllers setup

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersController } from './users/users.controller';
+import { PostsController } from './posts/posts.controller';
+import { HashtagController } from './hashtag/hashtag.controller';
 
 @Module({
   imports: [],
-  controllers: [AppController],
+  controllers: [AppController, UsersController, PostsController, HashtagController],
   providers: [AppService],
 })
 export class AppModule {}
