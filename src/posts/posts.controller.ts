aee0d74c78fa525f094171f7864c8b1dd6075b43
pankajkrbansal <pankajbansal.pkb@gmail.com>
2023-01-14 18:58:41 +0530

    Added controllers for posts

--- src/posts/posts.controller.ts
+++ src/posts/posts.controller.ts
@@ -1,4 +1,34 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Delete, Get, Param, Post, Put } from '@nestjs/common';
 
 @Controller('posts')
-export class PostsController {}
+export class PostsController {
+  @Get('/')
+  getAllPosts(): string {
+    return 'All Posts';
+  }
+
+  @Get('/:postid')
+  getPostById(@Param() param):string{
+    return ''
+  }
+
+  @Post('/')
+  createNewPost() : string{
+    return ''
+  }
+
+  @Delete('/:postid')
+  deletebyId(@Param() param):string{
+    return ''
+  }
+
+  @Put('/:postid/like')
+  likePostById(@Param() param):string{
+    return ''
+  }
+
+  @Put('/:postid/dislike')
+  dislikePostById():string{
+    return ''
+  }
+}
